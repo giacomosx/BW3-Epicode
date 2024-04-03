efc7fb41df7010dd6066090651b9d88d21247438
Andrea Zocco <75839392+AndreaZocco@users.noreply.github.com>
2024-04-03 20:16:18 +0200

    Add files via upload

---
+++ js/album.js
@@ -0,0 +1,24 @@
+// carica i dettagli dell'album e la lista delle tracce sulla pagina dell'album
+function loadAlbumDetails() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const albumId = urlParams.get('id');
+  
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`)
+      .then(response => response.json())
+      .then(album => {
+        // imposta i dettagli dell'album
+        document.querySelector(".container--album-info .album-title").innerText = album.title;
+        document.querySelector(".container--album-pic img").src = album.cover_medium;
+  
+        // pulisci e popola la lista delle tracce
+        const tracksListContainer = document.querySelector(".container--trakslist");
+        tracksListContainer.innerHTML = ''; 
+        album.tracks.data.forEach(track => {
+          tracksListContainer.innerHTML += `<div>${track.title}</div>`;
+        });
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', loadAlbumDetails);
+  
